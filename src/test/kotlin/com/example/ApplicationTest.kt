55088c0c49d6f37124cdac0c792cd6ff0cf1e31e
박형준 <mir8725@greencatsoft.com>
2022-02-06 22:55:40 +0900

    Initial commit

---
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,41 @@
+package com.example
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.http.content.*
+import io.ktor.server.webjars.*
+import java.time.*
+import io.ktor.server.auth.*
+import io.ktor.util.*
+import io.ktor.server.auth.jwt.*
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.server.locations.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.server.sessions.*
+import io.ktor.server.plugins.*
+import io.ktor.http.content.*
+import io.ktor.server.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import com.example.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ configureRouting() }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello World!", response.content)
+            }
+        }
+    }
+}
